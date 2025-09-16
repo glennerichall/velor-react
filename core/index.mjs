ba1c3de961edbce23c7f6eb21debc9ac4809ac75
Glenn Hall <glennerichall@hotmail.com>
2025-09-15 21:51:27 -0400

    re-re-fixed exported styles

--- core/index.mjs
+++ core/index.mjs
@@ -1,29 +1,27 @@
-
 import '../style/bootstrap.scss';
 import '../style/vanilla.scss';
 import '../style/widgets.scss';
 import '../style/slider.scss';
 
 
-export { default as Checkbox } from './Checkbox.jsx';
-export { default as Collapsible } from './Collapsible.jsx';
-export { default as Disable } from './Disable.jsx';
-export { default as DotNotification } from './DotNotification.jsx';
-export { default as Drawer } from './Drawer.jsx';
-export { default as DrawerContainer } from './DrawerContainer.jsx';
-export { default as GargantuaList } from './GargantuaList.jsx';
-export { default as MessageModal } from './MessageModal.jsx';
-export { default as ModalOkCancel } from './ModalOkCancel.jsx';
-export { default as PinButtons } from './PinButtons.jsx';
-export { default as Pinnable } from './Pinnable.jsx';
-export { default as Progress } from './Progress.jsx';
-export { default as RangeSelection } from './RangeSelection.jsx';
-export { default as RenderInWindow } from './RenderInWindow.jsx';
-export { default as Slider } from './Slider.jsx';
-export { default as TableContextMenu } from './TableContextMenu.jsx';
-export { default as TextField } from './TextField.jsx';
-export { Widget } from './Widget.jsx';
-export { WidgetGroup } from './Widget.jsx';
-export { default as Dropdown } from './Dropdown.jsx';
-
+export {default as Checkbox} from './Checkbox.jsx';
+export {default as Collapsible} from './Collapsible.jsx';
+export {default as Disable} from './Disable.jsx';
+export {default as DotNotification} from './DotNotification.jsx';
+export {default as Drawer} from './Drawer.jsx';
+export {default as DrawerContainer} from './DrawerContainer.jsx';
+export {default as GargantuaList} from './GargantuaList.jsx';
+export {default as MessageModal} from './MessageModal.jsx';
+export {default as ModalOkCancel} from './ModalOkCancel.jsx';
+export {default as PinButtons} from './PinButtons.jsx';
+export {default as Pinnable} from './Pinnable.jsx';
+export {default as Progress} from './Progress.jsx';
+export {default as RangeSelection} from './RangeSelection.jsx';
+export {default as RenderInWindow} from './RenderInWindow.jsx';
+export {default as Slider} from './Slider.jsx';
+export {default as TableContextMenu} from './TableContextMenu.jsx';
+export {default as TextField} from './TextField.jsx';
+export {Widget} from './Widget.jsx';
+export {WidgetGroup} from './Widget.jsx';
+export {default as Dropdown} from './Dropdown.jsx';
 export {default as ReactSlider} from './ReactSlider.jsx';
